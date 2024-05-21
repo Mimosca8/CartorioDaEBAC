cea9726426a763e2ab49e094a7f4d6f27f1cac04
Diego <mimosca8@gmail.com>
2024-05-21 10:31:36 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- Cartorio_de_DiegoSouza.c
+++ Cartorio_de_DiegoSouza.c
@@ -123,7 +123,8 @@ int main()
 	
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 	
 		printf("Opção: "); //fim do menu
 	
@@ -142,6 +143,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por ultilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
